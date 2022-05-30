9788ee640b1db2d860d34a5d6d5433f14e3ca0b8
Renato Cabral Rodrigues de Souza <CIANDT\renatocrds@lnb035014blz.ciandt.global>
2022-05-30 14:20:18 -0300

    tests may be wrong

--- src/test/java/com/pokemon/renato/resources/PokemonResourceTest.java
+++ src/test/java/com/pokemon/renato/resources/PokemonResourceTest.java
@@ -1,24 +1,47 @@
 package com.pokemon.renato.resources;
 
-import com.ciandt.pokemon.renato.domain.fixtures.PokemonResourceFixtures;
 import io.quarkus.test.junit.QuarkusTest;
-import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 @QuarkusTest
 public class PokemonResourceTest {
-
     @Test
     public void testAllPokemonEndpoint() {
         given()
                 .when().get("/pokemon/all")
-                .then().body((Matcher<?>) PokemonResourceFixtures.gimmePokemon())
+                .then()
                 .statusCode(200)
-                .body(is("hello"));
+                .body(not("Ocorreu um erro, por favor tente novamente"));
     }
 
+    @Test
+    public void testSinglePokemonEndpoint() {
+        given()
+                .when().get("/pokemon/1")
+                .then()
+                .statusCode(200)
+                .body(not("Pokemon não encontrado, por favor tente novamente"));
+    }
+
+    @Test
+    public void testNonExistingPokemonEndpointId() {
+        given()
+                .when().get("/pokemon/0")
+                .then()
+                .statusCode(200)
+                .body(is("Pokemon não encontrado, por favor tente novamente"));
+    }
+
+    @Test
+    public void testNonExistingPokemonEndpointName() {
+        given()
+                .when().get("/pokemon/misto")
+                .then()
+                .statusCode(200)
+                .body(is("Pokemon não encontrado, por favor tente novamente"));
+    }
 }
 
